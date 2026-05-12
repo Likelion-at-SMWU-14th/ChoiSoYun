9cafc8aceeec436d5628363b1b67e74eed70de79
xwuun <soyun7305@naver.com>
2026-05-12 20:20:13 +0900

    FEAT : CBV작성

--- django/week02/myinsta/urls.py
+++ django/week02/myinsta/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from posts.views import function_view, url_view, url_parameter_view
+from posts.views import function_view, url_view, url_parameter_view, class_view,class_view2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('url/', url_view),
     path('url/<str:username>/', url_parameter_view),
     path('fbv/', function_view),
+    path('cbv/', class_view.as_view()),
+    path('cbv2/', class_view2.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
