b4c57695e3cbb19368ddec3ddb43c2e5efbebb0e
xwuun <soyun7305@naver.com>
2026-05-17 15:22:22 +0900

    Feat : 온라인 감정 다이어리 구현

--- django/week02_hw/posts/models.py
+++ django/week02_hw/posts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Diary(models.Model):
-    title = models.CharField(verbose_name="제목", max_length=100)
-    content = models.TextField(verbose_name="내용")
+class MoodDiary(models.Model):
+    mood = models.CharField(verbose_name="감정", max_length=50)
+    content = models.TextField(verbose_name="일기 내용")
     created_at = models.DateTimeField(verbose_name="작성일", auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.mood
