9eb90aaa34ea62aeccf0fcf4e1c43ac0df9f0bf5
xwuun <soyun7305@naver.com>
2026-05-19 19:46:50 +0900

    Feat: Form 실습

---
+++ django/week03/myinsta/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from posts.views import function_view, url_view, url_parameter_view, class_view,class_view2, home_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('url/', url_view),
+    path('url/<str:username>/', url_parameter_view),
+    path('fbv/', function_view),
+    path('cbv/', class_view.as_view()),
+    path('cbv2/', class_view2.as_view()),
+    path('',home_view),
+
+    path('posts/', include('posts.urls', namespace='posts'))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
