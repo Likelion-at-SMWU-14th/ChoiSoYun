b3565ffd502e82e56c2d84218340933af381f57d
xwuun <soyun7305@naver.com>
2026-06-23 19:58:42 +0900

    Feat : @api_view+query param 계산기 구현

--- django/week04/myinsta/urls.py
+++ django/week04/myinsta/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls', namespace='posts'),)
+    path('calculate/query/', calculator_query),
 ]
 
 if settings.DEBUG:
--- django/week04/posts/views.py
+++ django/week04/posts/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from rest_framwork.veiwsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Post
 from .serializers import PostModelSerializer
 
+
 class PostModelViewSet(ModelViewSet):
-    queryset = post.objects.all()
+    queryset = Post.objects.all()
     serializer_class = PostModelSerializer
-# Create your views here.
