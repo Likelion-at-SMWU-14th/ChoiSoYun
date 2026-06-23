5a8b53db33a3f7e357f6cf81b4a0cdcbde4380c0
xwuun <soyun7305@naver.com>
2026-06-23 19:37:45 +0900

    Feat: PostModelViewSet 구현

--- django/week04/posts/views.py
+++ django/week04/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framwork.veiwsets import ModelViewSet
 
+from .models import Post
+from .serializers import PostModelSerializer
+
+class PostModelViewSet(ModelViewSet):
+    queryset = post.objects.all()
+    serializer_class = PostModelSerializer
 # Create your views here.
