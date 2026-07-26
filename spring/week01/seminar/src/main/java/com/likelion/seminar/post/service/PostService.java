8d1b18e44270598e5942b49499941e8ec91d60f3
xwuun <soyun7305@naver.com>
2026-07-26 21:16:10 +0900

    Feat:Post 삭제 기능 구현

--- spring/week01/seminar/src/main/java/com/likelion/seminar/post/service/PostService.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/service/PostService.java
@@ -38,5 +38,7 @@ public class PostService {
             targetPost.setWriter(postDTO.getWriter());
         }
     }
-
+    public void deletePost(int id) {
+        this.postDTOList.remove(id);
+    }
 }
