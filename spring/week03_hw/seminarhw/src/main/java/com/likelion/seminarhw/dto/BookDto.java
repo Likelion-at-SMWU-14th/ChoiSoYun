245beb1168b42adae0901fc09213d002a7b7eaf9
xwuun <soyun7305@naver.com>
2026-08-09 23:38:27 +0900

    Feat: REST로 Book 추가 및 조회

---
+++ spring/week03_hw/seminarhw/src/main/java/com/likelion/seminarhw/dto/BookDto.java
@@ -0,0 +1,17 @@
+package com.likelion.seminarhw.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+
+    private String title;
+    private String author;
+    private int price;
+}
--- spring/week03_hw/seminarhw/src/main/java/com/likelion/seminarhw/service/BookService.java
+++ spring/week03_hw/seminarhw/src/main/java/com/likelion/seminarhw/service/BookService.java
@@ -1,5 +1,6 @@
 package com.likelion.seminarhw.service;
 
+import com.likelion.seminarhw.dto.BookDto;
 import com.likelion.seminarhw.model.Book;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class BookService {
 
+    // MVC용
     private List<Book> books = new ArrayList<>();
 
     public void addBook(Book book) {
@@ -17,5 +19,18 @@ public class BookService {
 
     public List<Book> findAll() {
         return books;
+    }
+
+
+    // REST용
+    private List<BookDto> bookDtos = new ArrayList<>();
+
+    public BookDto addBookDto(BookDto bookDto) {
+        bookDtos.add(bookDto);
+        return bookDto;
+    }
+
+    public List<BookDto> findAllDto() {
+        return bookDtos;
     }
 }
