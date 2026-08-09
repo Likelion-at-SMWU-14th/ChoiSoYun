25a1b632539b9fe1e2534e329493d9976cad3f6c
xwuun <soyun7305@naver.com>
2026-08-09 23:21:21 +0900

    Feat: 3주차 프로과제 프젝 생성

---
+++ spring/week03_hw/seminarhw/src/main/java/com/likelion/seminarhw/SeminarhwApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.seminarhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarhwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeminarhwApplication.class, args);
+    }
+
+}
