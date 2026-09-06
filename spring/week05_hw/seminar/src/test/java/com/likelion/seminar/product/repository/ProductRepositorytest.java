ea858b0195c2381ce175e0ad9e34c44f3229aa50
xwuun <soyun7305@naver.com>
2026-09-06 21:25:28 +0900

    Feat: Product JPA JPQL QueryDSL 조회 구현

--- spring/week05_hw/seminar/src/test/java/com/likelion/seminar/product/repository/ProductRepositorytest.java
+++ spring/week05_hw/seminar/src/test/java/com/likelion/seminar/product/repository/ProductRepositorytest.java
@@ -1,6 +1,9 @@
 package com.likelion.seminar.product.repository;
 
 import com.likelion.seminar.product.domain.Product;
+import com.likelion.seminar.product.domain.QProduct;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,9 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @BeforeEach
     void setUp() {
         productRepository.saveAll(List.of(
@@ -32,12 +38,20 @@ class ProductRepositoryTest {
                 new Product("테이프", 1800, 90),
                 new Product("파일", 2200, 20),
                 new Product("스테이플러", 3000, 15),
-                new Product("펜케이스", 1900, 55)
+                new Product("펜케이스", 1900, 55),
+                new Product("수성펜", 700, 45),
+                new Product("네임펜", 900, 35),
+                new Product("붓펜", 1100, 25),
+                new Product("사인펜", 1300, 65),
+                new Product("중성펜", 1400, 75),
+                new Product("컬러펜", 1600, 85),
+                new Product("젤펜", 1700, 95)
         ));
     }
 
     @Test
     void 가장_비싼_상품_Top10을_조회한다() {
+
         // when
         List<Product> result =
                 productRepository.findTop10ByOrderByPriceDesc();
@@ -55,14 +69,15 @@ class ProductRepositoryTest {
                         2000,
                         1900,
                         1800,
-                        1500,
-                        1200,
-                        1000
+                        1700,
+                        1600,
+                        1500
                 );
     }
 
     @Test
     void 가격이_2000원_이하이고_재고가_많은_상품_Top5를_조회한다() {
+
         // given
         Pageable pageable = PageRequest.of(0, 5);
 
@@ -80,10 +95,46 @@ class ProductRepositoryTest {
                 .extracting(Product::getName)
                 .containsExactly(
                         "지우개",
+                        "젤펜",
                         "테이프",
-                        "샤프펜슬",
+                        "컬러펜",
+                        "샤프펜슬"
+                );
+    }
+
+    @Test
+    void 이름에_펜이_들어가고_가격이_저렴한_상품_Top10을_조회한다() {
+
+        // given
+        QProduct product = QProduct.product;
+
+        JPAQueryFactory queryFactory =
+                new JPAQueryFactory(entityManager);
+
+        // when
+        List<Product> result = queryFactory
+                .selectFrom(product)
+                .where(product.name.contains("펜"))
+                .orderBy(product.price.asc())
+                .limit(10)
+                .fetch();
+
+        // then
+        assertThat(result).hasSize(10);
+
+        assertThat(result)
+                .extracting(Product::getName)
+                .containsExactly(
+                        "수성펜",
+                        "네임펜",
+                        "볼펜",
+                        "붓펜",
                         "형광펜",
-                        "연필"
+                        "사인펜",
+                        "중성펜",
+                        "샤프펜슬",
+                        "컬러펜",
+                        "젤펜"
                 );
     }
 }
