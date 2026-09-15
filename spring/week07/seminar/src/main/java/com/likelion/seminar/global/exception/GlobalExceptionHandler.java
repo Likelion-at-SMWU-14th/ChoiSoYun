6461dc8b79f4259209ddd45f3e11883039d941a6
ChoiSoYun <soyun7305@naver.com>
2026-09-15 20:00:51 +0900

    Feat:CustomException 실습

--- spring/week07/seminar/src/main/java/com/likelion/seminar/global/exception/GlobalExceptionHandler.java
+++ spring/week07/seminar/src/main/java/com/likelion/seminar/global/exception/GlobalExceptionHandler.java
@@ -26,5 +26,12 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(messages);
     }
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException e) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+
 
 }
